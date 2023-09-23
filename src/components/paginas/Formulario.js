db3543315c8c3aefab98ae2a8a77365b139b943b
yoniedilzar <yoniedilzar@gmail.com>
2023-09-22 21:33:18 -0600

    menu de opciones

---
+++ src/components/paginas/Formulario.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Formulario = () => {
+  return (
+    <div className='py-5'>
+        <h1 className='py-5'>Formulario</h1>
+    </div>
+  )
+}
+
+export default Formulario
